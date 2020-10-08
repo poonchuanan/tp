4754cb56979e2b77d7137924428d621d62e093ab
chewyang <owenzhouyang98@gmail.com>
2020-10-08 22:12:34 +0800

    created super basic command class

---
+++ src/main/java/Command.java
@@ -0,0 +1,13 @@
+import seedu.duke.DayMap;
+
+public class Command {
+    protected DayMap dayMap;
+
+
+    //to be overriden
+    public void execute(){return;}
+
+    public void setData(DayMap dayMap) {
+        this.dayMap = dayMap;
+    }
+}
