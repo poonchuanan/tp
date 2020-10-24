2fb8887af29323baaa43893090ec3ed7dc23de41
e0425705 <e0425705@u.nus.edu>
2020-10-25 01:28:57 +0800

    add junit test

--- src/test/java/seedu/duke/ActivityEditTest.java
+++ src/test/java/seedu/duke/ActivityEditTest.java
@@ -1,6 +1,7 @@
 package seedu.duke;
 
 import org.junit.jupiter.api.Test;
+import seedu.duke.command.DeleteCommand;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -12,46 +13,48 @@ public class ActivityEditTest {
         dummyList.addActivity(new Food("Apple pie", 55, false));
         dummyList.addActivity(new Exercise("Juggle Apples", 100, false));
     }
-//
-//    @Test
-//    void editActivityFoodToExercise_success() {
-//        ActivityList dummyList = new ActivityList();
-//        createObjects(dummyList);
-//
-//        String userInput = "edita 1 e/ jumping c/ 100";
-//        new DeleteCommand(0);
-//        Parser command = new Parser(userInput);
-//        //String output = command.prepareEditActivityCommand(userInput);
-//        assertEquals("expected", "actual");
-//    }
 
-//    @Test
-//    void editActivityExerciseToFood_success() {
-//        ActivityList dummyList = new ActivityList();
-//        createObjects(dummyList);
-//
-//        Food food = new Food("Apple", 50, false);
-//        String output = food.toString();
-//        assertEquals(output, "[F] | Apple | 50");
-//    }
-//
-//    @Test
-//    void editActivityExerciseToExercise_success() {
-//        ActivityList dummyList = new ActivityList();
-//        createObjects(dummyList);
-//
-//        Food food = new Food("Apple", 50, false);
-//        String output = food.toString();
-//        assertEquals(output, "[F] | Apple | 50");
-//    }
-//
-//    @Test
-//    void editActivityFoodToFood_success() {
-//        ActivityList dummyList = new ActivityList();
-//        createObjects(dummyList);
-//
-//        Food food = new Food("Apple", 50, false);
-//        String output = food.toString();
-//        assertEquals(output, "[F] | Apple | 50");
-//    }
+    @Test
+    void editActivityFoodToExercise_success() {
+        ActivityList dummyList = new ActivityList();
+        createObjects(dummyList);
+
+        Activity activity = new Exercise("jumping",900, false);
+        dummyList.insertActivity(0, activity);
+
+        assertEquals("[E] | jumping | 900, [F] | Banana | 100, [F] | Apple pie | 55, [E] | Juggle Apples | 100", dummyList.toString());
+    }
+
+    @Test
+    void editActivityExerciseToFood_success() {
+        ActivityList dummyList = new ActivityList();
+        createObjects(dummyList);
+
+        Activity activity = new Food("Pineapple",77, false);
+        dummyList.insertActivity(3, activity);
+
+        assertEquals("[F] | Apple | 50, [F] | Banana | 100, [F] | Apple pie | 55, [F] | Pineapple | 77", dummyList.toString());
+    }
+
+    @Test
+    void editActivityExerciseToExercise_success() {
+        ActivityList dummyList = new ActivityList();
+        createObjects(dummyList);
+
+        Activity activity = new Exercise("jumping",900, false);
+        dummyList.insertActivity(3, activity);
+
+        assertEquals("[F] | Apple | 50, [F] | Banana | 100, [F] | Apple pie | 55, [E] | jumping | 900", dummyList.toString());
+    }
+
+    @Test
+    void editActivityFoodToFood_success() {
+        ActivityList dummyList = new ActivityList();
+        createObjects(dummyList);
+
+        Activity activity = new Food("Pineapple",77, false);
+        dummyList.insertActivity(2, activity);
+
+        assertEquals("[F] | Apple | 50, [F] | Banana | 100, [F] | Pineapple | 77, [E] | Juggle Apples | 100", dummyList.toString());
+    }
 }
