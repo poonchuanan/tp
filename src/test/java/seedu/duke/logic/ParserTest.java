3ecd34aab43f27e6ec9e69ed2aa6b4e9ec7cd8eb
1-karthigeyan-1 <karthigeyan98@yahoo.com.sg>
2020-10-28 04:11:04 +0800

    add parsertest

---
+++ src/test/java/seedu/duke/logic/ParserTest.java
@@ -0,0 +1,35 @@
+package seedu.duke.logic;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import seedu.duke.command.Command;
+import seedu.duke.command.DeleteCommand;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParserTest {
+
+    @Test
+    public void deleteAll_success() {
+        Parser parser = new Parser("delete all/");
+        Command cmd = parser.parseCommand();
+        assertTrue(cmd instanceof DeleteCommand);
+    }
+
+    @Test
+    public void deleteIndex_success() {
+        Parser parser = new Parser("delete 2");
+        Command cmd = parser.parseCommand();
+        assertTrue(cmd instanceof DeleteCommand);
+    }
+
+    @Test
+    public void deleteNull_Exception() {
+        Parser parser = new Parser("delete");
+
+        Assertions.assertThrows(NumberFormatException.class, () -> {
+            Command cmd = parser.parseCommand();
+        });
+    }
+
+}
