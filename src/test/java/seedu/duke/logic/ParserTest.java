d8f58c15df6bc930914db28002dd0ea6a025047f
1-karthigeyan-1 <karthigeyan98@yahoo.com.sg>
2020-10-28 21:58:57 +0800

    fix checkstyle

--- src/test/java/seedu/duke/logic/ParserTest.java
+++ src/test/java/seedu/duke/logic/ParserTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import seedu.duke.command.Command;
 import seedu.duke.command.DeleteCommand;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ParserTest {
 
