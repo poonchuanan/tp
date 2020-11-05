34bd873bccf17d73f8a2a635d56cea1ed6b9d333
1-karthigeyan-1 <karthigeyan98@yahoo.com.sg>
2020-11-05 15:39:50 +0800

    change user commands

---
+++ src/main/java/seedu/duke/logic/preparecommand/PrepareUserCommand.java
@@ -0,0 +1,35 @@
+package seedu.duke.logic.preparecommand;
+
+import seedu.duke.command.Command;
+import seedu.duke.command.CreateNewUserCommand;
+import seedu.duke.exception.InvalidNumberOfArguments;
+
+public class PrepareUserCommand extends PrepareCommand {
+    private static final int ARGUMENT_LIMIT = 2;
+    public PrepareUserCommand(String[] description) {
+        super(description);
+    }
+
+    @Override
+    public Command prepareCommand() throws Exception {
+        try {
+            isNumberOfArgumentsValid(ARGUMENT_LIMIT);
+        } catch (InvalidNumberOfArguments e) {
+            System.out.println("Arguments are not valid!");
+        }
+        String[] input = description[1].split(" ", 2);
+        PrepareCommand command;
+        switch(input[0].trim()) {
+        case "l/":
+            return new PrepareProfileListCommand(input).prepareCommand();
+        case "c/":
+            return new CreateNewUserCommand();
+        case "e/":
+            return new PrepareEditUserProfile(input).prepareCommand();
+        default:
+            return null;
+            }
+    }
+
+
+}
