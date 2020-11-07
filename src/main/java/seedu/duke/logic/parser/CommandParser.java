089901dc6aac52b194922ef04d3d7a9a2dd9f7f8
1-karthigeyan-1 <karthigeyan98@yahoo.com.sg>
2020-11-07 16:43:10 +0800

    Removed old profile commands

--- src/main/java/seedu/duke/logic/parser/CommandParser.java
+++ src/main/java/seedu/duke/logic/parser/CommandParser.java
@@ -62,16 +62,12 @@ public class CommandParser extends Parser {
                 return new PrepareAddSetCommand(arguments).prepareCommand();
             case "find":
                 return new PrepareFindCommand(arguments).prepareCommand();
-            case "edit":
-                return new PrepareEditUserProfile(arguments).prepareCommand();
             case "edita":
                 return new PrepareEditCommand(arguments).prepareCommand();
             case "delete":
                 return new PrepareDeleteCommand(arguments).prepareCommand();
             case "list":
                 return new PrepareListCommand(arguments).prepareCommand();
-            case "listup":
-                return new PrepareUserListCommand(arguments).prepareCommand();
             case "help":
                 return new PrepareHelpCommand(arguments).prepareCommand();
             case "move":
